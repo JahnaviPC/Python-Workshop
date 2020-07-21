a765c6d4815253ba59d61a8867ba2ff7e4d54441
Jahnavi P C <43513317+JahnaviPC@users.noreply.github.com>
2020-07-21 20:13:09 +0530

    Create AddTwoNum.py

---
+++ AddTwoNum.py
@@ -0,0 +1,17 @@
+#Python Program to Add Two Numbers getting through key board
+
+# sum of two nos
+num1 = int(input("Enter first no"))
+num2 = int(input("Enter second no"))
+
+# Adding the two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+OUTPUT:
+Enter first no2
+Enter second no3
+The sum of 2 and 3 is 5
