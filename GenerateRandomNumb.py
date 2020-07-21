a67fa1a8cb620ed140d1707091aed0217baa9f0f
Jahnavi P C <43513317+JahnaviPC@users.noreply.github.com>
2020-07-21 20:18:11 +0530

    Create GenerateRandomNumb.py

---
+++ GenerateRandomNumb.py
@@ -0,0 +1,8 @@
+Python Program to Generate a Random Number
+
+# Program to generate a random number between 0 and 9
+# import the random module
+import random
+print(random.randint(0,9))
+  
+OUTPUT:8
