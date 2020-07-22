ee991e99b95881eb204f62717e87568293c38ae0
Jahnavi P C <43513317+JahnaviPC@users.noreply.github.com>
2020-07-22 11:33:29 +0530

    Create GreaterOfTwoNumbers.py

---
+++ GreaterOfTwoNumbers.py
@@ -0,0 +1,9 @@
+Find the greater of two nos
+
+p = int(input("enter the first number:"))
+n = int(input("enter the second number:"))
+
+if (p > n):
+  print("The first number is greater")
+else:
+  print("the second number is greater")
