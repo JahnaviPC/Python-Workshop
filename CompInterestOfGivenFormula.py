d72110007d43f48b86279397db76934fe7f52976
Jahnavi P C <43513317+JahnaviPC@users.noreply.github.com>
2020-07-22 11:29:58 +0530

    Create CompInterestOfGivenFormula.py

---
+++ CompInterestOfGivenFormula.py
@@ -0,0 +1,13 @@
+Find the compound interest for the given p,n,r (formula : p(1+n*r/100)n )
+
+p = float(input("enter the priciple amount:"))
+n = float(input("enter time period"))
+r = float(input("enter the rate of intrest"))
+
+print ("the compund intrest is: ", p*(1+n*r/100)*n)
+
+OUTPUT:
+enter the priciple amount:200
+enter time period25
+enter the rate of intrest0.9
+the compund intrest is:  6125.000000000001
