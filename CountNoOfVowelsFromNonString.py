b0a2cba9809996f74c6f456cc8c8af01b3c7a2ce
Jahnavi P C <43513317+JahnaviPC@users.noreply.github.com>
2020-07-22 11:18:46 +0530

    Create CountNoOfVowelsFromNonString.py

---
+++ CountNoOfVowelsFromNonString.py
@@ -0,0 +1,14 @@
+Python Program to count number of vowels from a non-empty string.
+
+string=input("Enter string:")
+vowels=0
+for i in string:
+      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+            vowels=vowels+1
+print("Number of vowels are:")
+print(vowels)
+
+OUTPUT:
+Enter string:permitted
+Number of vowels are:
+3
