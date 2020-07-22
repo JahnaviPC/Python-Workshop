6ee2a13f6ab06abd16b6ff0a7f71ff797158835f
Jahnavi P C <43513317+JahnaviPC@users.noreply.github.com>
2020-07-22 11:10:54 +0530

    Create Replace'a'and$.py

---
+++ Replace'a'and$.py
@@ -0,0 +1,13 @@
+Python Program to replace all occurrences of ‘a’ with ‘$’ in a string.
+
+string=input("Enter string:")
+string=string.replace('a','$')
+string=string.replace('A','$')
+print("Modified string:")
+print(string)
+
+
+OUTPUT:
+Enter string:mamam
+Modified string:
+m$m$m
