a3191457eb62a9a599a8376c49be04406aa70201
Jahnavi P C <43513317+JahnaviPC@users.noreply.github.com>
2020-07-22 11:31:52 +0530

    Create CentegrateToFarenheit.py

---
+++ CentegrateToFarenheit.py
@@ -0,0 +1,10 @@
+Convert centegrade to farenheit
+
+#( f= 9/5*c+32)
+c = float(input("Enter the centigrade value to be converted:"))
+
+print("the converted value of centigrade to farenheit is:",(1.8*c)+32)
+
+OUTPUT:
+Enter the centigrade value to be converted:0
+the converted value of centigrade to farenheit is: 32.0
